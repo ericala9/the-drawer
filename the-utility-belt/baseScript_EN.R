e401cf2cb04c560cf1af0e12be9e9ceadf8a2fcc
Erica Ambrosio <152650668+ericala9@users.noreply.github.com>
2025-11-10 14:28:12 -0300

    Update baseScript_EN.R

--- the-utility-belt/baseScript_EN.R
+++ the-utility-belt/baseScript_EN.R
@@ -28,12 +28,14 @@ require(reshape2)
 require(stringr)
 
 # Function to create centered section titles
-center_title <- function(title, width=78, border = "-", uppercase = FALSE) {
+center_title <- function(title, width = 78, border = "-", uppercase = FALSE) {
   if (uppercase) title <- toupper(title)
-  top_bottom <- paste(rep(border, width), collapse = "")
-  pad <- floor((width - nchar(title))/2)
-  middle <- paste0(strrep(" ", pad-1), title)
-  cat("#", top_bottom, "\n# ", middle, "\n#", top_bottom, "\n")
+  label <- paste0(" ", title, " ")
+  pad_total <- width - nchar(label)
+  pad_left <- floor(pad_total / 2)
+  pad_right <- ceiling(pad_total / 2)
+  line <- paste0("# ", strrep(border, pad_left), label,strrep(border, pad_right))
+  cat(line, "\n")
 }
 
 # center_title("Data Preparation")
