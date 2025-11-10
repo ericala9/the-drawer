63724b9781c2275e9e948466a45e03cbde1d69db
Erica Ambrosio <152650668+ericala9@users.noreply.github.com>
2025-11-10 14:26:58 -0300

    Update baseScript_PT.R

--- the-utility-belt/baseScript_PT.R
+++ the-utility-belt/baseScript_PT.R
@@ -28,12 +28,14 @@ require(reshape2)
 require(stringr)
 
 # Função para criar títulos centralizados de seções.
-centralizado <- function(title, width=78, border = "-", uppercase = FALSE) {
+centralizado <- function(title, width = 78, border = "-", uppercase = FALSE) {
   if (uppercase) title <- toupper(title)
-  top_bottom <- paste(rep(border, width), collapse = "")
-  pad <- floor((width - nchar(title))/2)
-  middle <- paste0(strrep(" ", pad-1), title)
-  cat("#", top_bottom, "\n# ", middle, "\n#", top_bottom, "\n")
+  label <- paste0(" ", title, " ")
+  pad_total <- width - nchar(label)
+  pad_left <- floor(pad_total / 2)
+  pad_right <- ceiling(pad_total / 2)
+  line <- paste0("# ", strrep(border, pad_left), label,strrep(border, pad_right))
+  cat(line, "\n")
 }
 
 # centralizado("Preparação dos dados")
